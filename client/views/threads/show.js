5e1406556a47f2fb3b1e0d6de94c5207a4c4bb81
Nathan Ross <nathantross@gmail.com>
2014-04-11 10:42:17 -0700

    Scroll starts at bottom of threads

---
+++ client/views/threads/show.js
@@ -0,0 +1,8 @@
+function scrollFix() {   
+  var objDiv = document.getElementById("messages");
+  objDiv.scrollTop = objDiv.scrollHeight;
+  setTimeout("scrollFix();", 500);
+}
+window.onload = function() { scrollFix(); }
+
+
