cae177066bd9c650367ad79f2b0691c41f5eef42
stuartstein <stuart.stein@gmail.com>
2014-06-16 19:05:22 -0700

    Changed Kadira secret key and hid from git

--- server/kadira.js
+++ server/kadira.js
@@ -1,3 +1,3 @@
 Meteor.startup(function() {
-  Kadira.connect('5pc9oWbsodjBuJJMB', '5222458e-8282-4e4d-8b77-d2743656ba7c');
+  Kadira.connect('5pc9oWbsodjBuJJMB', 'ee3c2d9f-e956-46cd-aa4c-0547af4f1856');
 });
